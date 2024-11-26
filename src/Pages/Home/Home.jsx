ac27d2c112d47505a39d180f07b76c69bc82f405
kiran pandey <kiranmmm138@gmail.com>
2024-11-26 14:06:39 +0545

    Add Hero Section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 import "./Home.css";
-import Navbar from "../../components/Navbar/Navbar"
+import Navbar from "../../components/Navbar/Navbar";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
+import TitleCards from "../../components/TitleCards/TitleCards";
+
 const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className="caption-img" />
+          <p>
+            Discovering the ties to a secret anicient order, ayoung man living
+            in modern Istanbul embarks on a quest to save city from an immortal
+            enemy.
+          </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt=""  />Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="" />More Info
+            </button>
+          </div>
 
-  return(
-
-<div className="home">
-<Navbar/>
-</div>
-)
-
-    
+            <TitleCards/>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import "./TitleCards.css"
+import cards_data  from "../../assets/cards/Cards_data"
+
+
 const TitleCards = () => {
+  
+  const cardsRef = useRef();
+
+const handleWheel = (e) => {
+  e.preventDefault();
+  cardsRef.current.scrollLeft += e.deltaY;
+}
+
+useEffect( () => {
+cardsRef.current.addEventListener('wheel', handleWheel);
+},[])
+
   return (
-    <div className='titlecard'>TitleCards</div>
+    <div className='titlecard'>
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map( (card,index) => {
+            return <div className='card' key={index}>
+              <img src= {card.image} alt="" />
+              <p>{card.name}</p>
+              </div>
+        })}
+      </div>
+    </div>
   )
 }
 
